711f1531f1dd77ecf87d1fbc30351c7bfa3bc999
AdrianHTF <a.fien@gmx.de>
2018-06-24 11:56:51 +0200

    log must be manually activated

--- src/main/java/de/htwg/se/connectfour/mvc/persistence/MongoDB.scala
+++ src/main/java/de/htwg/se/connectfour/mvc/persistence/MongoDB.scala
@@ -9,12 +9,14 @@ import de.htwg.se.connectfour.mvc.controller.{Controller}
 object MongoDB extends DaoMongo [Cell, Long] with LazyLogging {
   type T = Unit
 
+  val log = false
+
   val mongoConn = MongoConnection()
   val mongoColl = mongoConn("casbah_test")("test_data")
 
   override def create(cell: Cell): T = {
     mongoColl += MongoDBObject("x" -> cell.x, "y" -> cell.y, "typ" -> cell.cellType.toString)
-    //logger.info(s"MongoDB: Saved ${cell.debug}")
+    if (log) logger.info(s"MongoDB: Saved ${cell.debug}")
   }
 
   override def read(controller: Controller): Unit = {
@@ -22,18 +24,20 @@ object MongoDB extends DaoMongo [Cell, Long] with LazyLogging {
     val seq = Seq.empty[Cell]
 
     for { c <- mongoColl} {
-      //logger.info(s"Old seq: ${seq.size}")
-      //logger.info(s"c: $c")
+      if (log) {
+        logger.info(s"Old seq: ${seq.size}")
+        logger.info(s"c: $c")
+      }
       val x = Int.unbox(c.get("x"))
-      //logger.info(s"x: $x")
+      if (log) logger.info(s"x: $x")
       val y = Int.unbox(c.get("y"))
-      //logger.info(s"y: $y")
+      if (log) logger.info(s"y: $y")
       val typ = Cell.toCellType(String.valueOf(c.get("typ")))
-      //logger.info(s"typ: $typ")
+      if (log) logger.info(s"typ: $typ")
       val cell = Cell(x, y, typ)
-      //logger.info(s"cell: ${cell.debug}")
+      if (log) logger.info(s"cell: ${cell.debug}")
       seq :+ cell
-      //logger.info(s"New seq: ${seq.size}\n")
+      if (log) logger.info(s"New seq: ${seq.size}\n")
       controller.grid.set(x, y, typ)
     }
 
